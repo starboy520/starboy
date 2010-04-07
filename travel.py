123af1c899616095046e19322348397726da0824
starboy.qi <starboy@localhost.localdomain>
2010-04-07 09:21:20 +0800

    sadasd

--- travel.py
+++ travel.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 
 #Traveling Salesman Problem
-#there is only element in s: c(s,k) = d[0][k]
-#                            c(s,k) = min{c(s/{k},m)+d[m][k]}    m in s/{k}
+#s is the set that contain all the points.
+#c(s,k) represent point 1 travel the set point in set s with min length, and stop at point k;
+#if there is only element k in s: c(s,k) = d[0][k]
+#                            else:  c(s,k) = min{c(s/{k},m)+d[m][k]}    m in s/{k}
 s = [1,2,3,4,5]
 d = [[0,10,20,30,40,50],[12,0,18,30,25,21],[23,19,0,5,10,15],[34,32,4,0,8,16],[45,27,11,10,0,18],[56,22,26,20,12,0]]
 
