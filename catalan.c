cf649aa83a43ee58cf002cbaab2f91d49f053784
starboy <starboy.qi@gmail.com>
2011-06-25 08:48:12 +0800

    add new stiring

--- catalan.c
+++ catalan.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 //卡特兰数  c(2n,n) -c(2n,n+1) = 1/n+1 *c(2n,n)
 // 
-int katalan(int n) {
-    int i;
-    int a = n+2;
-    int b = 2;
-    int value = 1;
+long long int catalan(int n) {
+    long long int  i;
+    long long int  a = n+2;
+    long long int  b = 2;
+    long long int  value = 1;
     while (a <= 2*n) {
         value *= a;
         a++;
@@ -23,7 +23,11 @@ int katalan(int n) {
 
 int main() {
     int n = 5;
-    printf("%d\n", katalan(n));
+    printf("%lld\n", catalan(n));
+    printf("%lld\n", catalan(4));
+    printf("%lld\n", catalan(1));
+    printf("%lld\n", catalan(2));
+    printf("%lld\n", catalan(20));
     return 0;
 }
 
