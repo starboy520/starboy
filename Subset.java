8e4cd3adfe7f88144e76d90cd6b23bd16870c651
starboy <starboy.qi@gmail.com>
2011-08-01 21:58:26 +0800

    add a new file

---
+++ Subset.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// Give a set of integer
+// input all the subset of this array
+public class Subset {
+	public static List< List<Integer> > getSubset(List<Integer> set) {
+		int size = set.size();
+		int max = 1 << size;
+		List<List<Integer>> allsubsets = new ArrayList<List<Integer>>();
+		for (int i = 0; i < max; i++) {
+			List<Integer> oneset = new ArrayList<Integer>();
+			int k = i;
+			int index = 0;
+			while ( k > 0) {
+				if ((k & 1) > 0) {
+					oneset.add(set.get(index));
+				}
+				index++;
+				k >>= 1;
+			}
+			allsubsets.add(oneset);
+		}
+		return allsubsets;
+	}
+	
+	public static List< List<Integer> > getSubset(List<Integer> set, int index) {
+		List< List<Integer> > allsubset = new ArrayList<List<Integer>>();
+		if (index == set.size()) {
+			allsubset.add(new ArrayList<Integer>());
+		} else {
+			List< List<Integer> > currentSubset = getSubset(set, index+1);
+			for (List<Integer> oneset : currentSubset) {
+				List<Integer> thisset = new ArrayList<Integer>();
+				int num = set.get(index);
+				thisset.addAll(oneset);
+				thisset.add(num);
+				allsubset.add(thisset);
+			}
+			allsubset.addAll(currentSubset);
+		}
+		
+		return allsubset;
+	}
+	
+	public static void main(String[] args) {
+		List<Integer> a = Arrays.asList(1,2,3);
+		System.out.println(getSubset(a));
+		//System.out.println(getSubset(a, 0));
+	}
+}
