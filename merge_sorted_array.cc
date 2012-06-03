6aebcdb1e78d273e11b1b4394770ebd6ffb0e92f
unknown <BAIDU@BAIDU-PC.(none)>
2012-06-03 20:03:23 +0800

    merge two sorted array without extra space

--- merge_sorted_array.cc
+++ merge_sorted_array.cc
@@ -1,64 +1,47 @@
-//数组a[0, mid-1] 和 a[mid, num-1] 对数组a[0, num-1] 的两个子有序进行merge, 得到a[0, num-1]有序, 空间复杂度为o(1)
-//
+// how to merge two sorted arrays provided not to use a third array nor you can allocate
+// extra space try to optimize the problem to the best, time complexity should be less than O(n^2)
+
+// algorithm:
+//    just see the code.
 #include <stdio.h>
 #include <algorithm>
 using namespace std;
-
-void reversed(int* a, int s, int e) {
-  if (NULL == a || s > e-1)
-    return;
-
-  e = e - 1;
-  while (s < e) {
-    int k = s;
-    s = e;
-    e = k;
-    s++;
-    e--;
-  }
-}
 
 void rotateRight(int*a, int s, int e, int step) {
   if (NULL == a || s > e-1)
     return;
 
-  step %= e-s;
+  //step %= e-s;
 
   if (step == 0) 
     return;
 
-  reversed(a, s, e-step);
-  reversed(a, e-step, e);
-  reversed(a, s, e);
+  reverse(a, s, e-step);
+  reverse(a, e-step, e);
+  reverse(a, s, e);
 }
 
 
 
 void sortedListMerge(int* a, int num, int mid) {
-  if (NULL == a || mid > num || 0 >= mid) return ;
-
-  int i = 0;
-  int j = mid;
-
-  while (i < num) {
-    while (a[i] < a[j]) {
-      i++;
-    }
+    if (NULL == a || mid > num || 0 >= mid)
+        return ;
     
-    int step = j;
+    int i = 0;
+    int j = mid;
+    
+    while (i < num) {
+        while (a[i] < a[j]) {
+            i++;
+        }
+        int step = j;
+    
+        while (a[j] < a[i]) {
+            j++;
+        }
 
-    while (a[j] < a[i]) {
-      j++;
+        step = j - step;
+        rotateRight(a, i, j, j-step);
     }
-
-    step = j - step;
-
-    rotateRight(a, i, j, j-step);
-  }
 
 }
-
-
-
-
-
