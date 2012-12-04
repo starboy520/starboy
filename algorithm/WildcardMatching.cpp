efd314231b8607bf496bfe690a31ce54a9bfdb67
qichengjie <starboy.qi@gmail.com>
2012-12-04 23:45:19 +0800

    Update algorithm/WildcardMatching.cpp
    ugly, but seem right?

--- algorithm/WildcardMatching.cpp
+++ algorithm/WildcardMatching.cpp
@@ -1,34 +1,48 @@
+
 #include <iostream>
 using namespace std;
 
 bool isMatch(const char* s, const char* p) {
-
-  if ((*p == '\0') && (*s == '\0')) {
-    return true;
-  } else if (
-      ( (*p == '\0') && (*s != '\0')) ) {
-    return false;
-  } else {
-    if ((*p != '*') && (*p != '?')) {
-      if (*p == *s) {
-        return isMatch(s+1, p+1);
-      } else {
-        return false;
-      }
-    } else if (*p == '?') {
-      return isMatch(s+1, p+1);
-    } else {
-      return (isMatch(s, p+1) || isMatch(s+1, p) || isMatch(s+1, p+1));
+    if (*p == '\0' && *s == '\0') {
+        return true;
     }
-  }
-  return false;
+
+    if (*p == '\0' && (*s != '\0')) {
+        return false;
+    }
+
+    if (*s == '\0' && *p != '*') {
+        return false;
+    }
+
+
+    if ((*p != '*') && (*p != '?')) {
+        if (*p == *s) {
+            return isMatch(s+1, p+1);
+        } else {
+            return false;
+        }
+    } else if (*p == '?') {
+        return isMatch(s+1, p+1);
+    } else {
+        if (isMatch(s, p+1)) {
+            return true;
+        } else {
+            if (*s != '\0') {
+                return isMatch(s+1, p) || isMatch(s+1, p+1);
+            }
+        }
+    }
+    return false;
 }
 
 int main() {
-  char s[] = "aaab";
-  char p[] = "a*b";
-  if (isMatch(s, p))
+  char s[] = "babaaababaabababbbbbbaabaabbabababbaababbaaabbbaaab";
+  char p[] = "***bba**a*bbba**aab**b";
+  if (isMatch(s, p)) {
     cout << "yes" << endl;
-
+  } else {
+    cout << "no" << endl;
+  }
   return 0;
 }
