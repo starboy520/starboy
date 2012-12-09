ae9823c60b22a4a43524f1d055db656257af2888
Starboy <starboy.qi@gmail.com>
2012-12-09 23:27:15 +0800

    stole code frome boost

--- algorithm/WildcardMatching.cpp
+++ algorithm/WildcardMatching.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 using namespace std;
 
+// this is too ugly
+//
 bool isMatch(const char* s, const char* p) {
     if (*p == '\0' && *s == '\0') {
         return true;
@@ -34,6 +36,65 @@ bool isMatch(const char* s, const char* p) {
         }
     }
     return false;
+}
+
+bool isMatch2(const char* p, const char*p) {
+  // when original string is empty
+  // only empty pattern or pattern with only asterisks would match it
+  //
+  if (*s == '\0') {
+    while (*p != '\0' && *p == '*')
+      ++p;
+    return (*p == '\0') ? true : false;
+  }
+
+  // original string is not empty, but pattern is empty,
+  // not match;
+  if (*p == '0') {
+    return false;
+  }
+
+  bool hasAsterisk = false;
+  while (*p != '\0') {
+    if (*p == '*') {
+      p++;
+      hasAsterisk = true;
+      continue;
+    }
+
+    const char* ss = s;
+    const char* sp = p;
+    while (*s != '\0' && (*p != '\0' && *p != '*')) {
+      if (*s == *p || *p == "?") {
+        ++s;++p;
+      } else {
+        s = ++ss; p = sp;
+      }
+    }
+
+    // if string not exist, check if rest pattern is either empty or only
+    // contains asteisks
+    if (*s == '\0') {
+      return isMatch2(s, p);
+    }
+  }
+
+  // original string is not empty, match last normal string with pattern from
+  // tail, if not match , the pattern could not match *whole* string
+  //
+  if (*s != '\0') {
+    if (!hasAsterisk) {
+      return false;
+    }
+    if (*(--p) == '*') return true;
+    while (s[1] != '\0') ++s;
+
+    while (*p != '*' && (*s == *p || *p == '?')) --s, --p;
+
+    return (*p == '*'): true : false;
+  }
+
+  return true;
 }
 
 int main() {
---
+++ gnu_private/any.hpp
@@ -0,0 +1,83 @@
+// this code is stolen from boost
+#include <algorithm>
+#include <typeinfo>
+
+class any {
+ public:
+  any() : content(0) {
+  }
+
+  ~any() {
+    delete content;
+  }
+
+  template <typename ValueType>
+  any(const ValueType& value) :
+      content (new holder<ValueType>(value)) {
+  }
+
+  any(const any& other) :
+      content(other.content ? other.content->clone() : 0) {
+  }
+
+ public:
+  any& swap(any& rhs) {
+    std::swap(content, rhs.content);
+    return *this;
+  }
+
+  const std::type_info & type() const {
+    return content ? content->type() : typeid(void);
+  }
+
+  template <typename ValueType>
+  any& operator=(const ValueType& rhs) {
+    any(rhs).swap(*this);
+    return *this;
+  }
+
+ public:
+  bool empty() const {
+    return !content;
+  }
+
+ public:
+  class placeholder {
+   public:
+    virtual ~placeholder() {
+    }
+
+   public:
+    virtual const std::type_info& type() const = 0;
+    virtual placeholder* clone() const = 0;
+  };
+
+  template <typename ValueType>
+  class holder : public placeholder {
+   public:
+    holder(const ValueType& value) :
+        held(value) {
+    }
+   public:
+    virtual const std::type_info& type() const {
+      return typeid(ValueType);
+    }
+
+    virtual placeholder* clone() const {
+      return new holder(held);
+    }
+   public:
+    ValueType held;
+
+   private:
+    holder& operator=(const holder&);
+  };
+
+  placeholder* content;
+};
+
+template <typename ValueType>
+ValueType* any_cast(any* operand) {
+  return operand && operand->type() == typeid(ValueType) ?
+      &static_cast<any::holder<ValueType>*>(operand->content)->held :0;
+}
