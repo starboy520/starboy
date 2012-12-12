5b20e27eceaf08ce5e53df42765a26409971d822
Chengjie Qi <starboy.qi@gmail.com>
2012-12-12 21:09:09 +0800

    And Combinations

--- leetcode/Anagrams.cpp
+++ leetcode/Anagrams.cpp
@@ -5,49 +5,49 @@
 
 using namespace std;
 
-	int my_compare (const string& i, const string& j) {
-		string temp1 = i;
-		string temp2 = j;
-		std::sort(temp1.begin(), temp1.end());
-		std::sort(temp2.begin(), temp2.end());
-		if (temp1 == temp2) return 0;
-		return -1;
-	};
+int my_compare (const string& i, const string& j) {
+	string temp1 = i;
+	string temp2 = j;
+	std::sort(temp1.begin(), temp1.end());
+	std::sort(temp2.begin(), temp2.end());
+	if (temp1 == temp2) return 0;
+	return -1;
+};
 
-	int split(vector<string>& strs, int begin, int high) {
-		string& x = strs[begin];
+int split(vector<string>& strs, int begin, int high) {
+	string& x = strs[begin];
 
-		int i = begin;
+	int i = begin;
 
-		for (int j = begin+1; j <= high; j++) {
-			if (my_compare(strs[j], x) == 0) {
-				i++;
-				std::swap(strs[i], strs[j])	;
-			}
+	for (int j = begin+1; j <= high; j++) {
+		if (my_compare(strs[j], x) == 0) {
+			i++;
+			std::swap(strs[i], strs[j])	;
 		}
-		return i;
 	}
+	return i;
+}
 
-    vector<string> anagrams(vector<string> &strs) {
-		int low = 0;
-		int high = strs.size() -1;
-		while (low < high) {
-			int idx = split(strs, low, high);
-			if (idx == low) {
-				cout << "12323" << endl; 
-				std::swap(strs[idx], strs[high]);
-				high--;
-				low = idx;
-			} else {
-				low = idx + 1;
-			}
+vector<string> anagrams(vector<string> &strs) {
+	int low = 0;
+	int high = strs.size() -1;
+	while (low < high) {
+		int idx = split(strs, low, high);
+		if (idx == low) {
+			cout << "12323" << endl; 
+			std::swap(strs[idx], strs[high]);
+			high--;
+			low = idx;
+		} else {
+			low = idx + 1;
 		}
-		vector<string> result;
-		for (int j = 0; j < low; j++) {
-			result.push_back(strs[j]);
-		}
-		return result;
 	}
+	vector<string> result;
+	for (int j = 0; j < low; j++) {
+		result.push_back(strs[j]);
+	}
+	return result;
+}
 
 int main() {
 	vector<string> strs;
