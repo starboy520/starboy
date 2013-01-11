62a2b641a646a23c06410525d1bea357af9efa0b
Starboy <starboy.qi@gmail.com>
2013-01-11 11:41:44 +0800

    code optimization

--- leetcode/UniqueBinarySearchTreesII.cpp
+++ leetcode/UniqueBinarySearchTreesII.cpp
@@ -11,52 +11,30 @@ class Solution {
 public:
     vector<TreeNode* > gen(int begin, int end) {
         vector<TreeNode*> result;
-        if (begin > end) return result;
+        if (begin > end) {
+          result.push_back(NULL);
+          return result;
+        }
 
-        
         for (int i = begin; i <= end; i++) {
             vector<TreeNode*> tmpall;
             vector<TreeNode*> tmp1 = gen(begin, i-1);
             vector<TreeNode* >tmp2 = gen(i+1, end);
-            
-            if (tmp1.size() == 0 && tmp2.size() == 0) {
+
+
+            for (int j = 0; j < tmp1.size(); j++) {
+              for (int k = 0; k < tmp2.size(); k++) {
                 TreeNode* c = new TreeNode(i);
+                c->left = tmp1[j];
+                c->right = tmp2[k];
                 result.push_back(c);
-                
-            } else if (tmp1.size() == 0) {
-                for (int j = 0; j < tmp2.size(); j++) {
-                    TreeNode* c = new TreeNode(i);
-                    c->right = tmp2[j];
-                    result.push_back(c);
-                }
-            } else  if (tmp2.size() == 0) {
-                for (int j = 0; j < tmp1.size(); j++) {
-                    TreeNode* c = new TreeNode(i);
-                    c->left = tmp1[j];
-                    result.push_back(c);
-                }
-            }  else {
-            
-                for (int j = 0; j < tmp1.size(); j++) {
-                    for (int k = 0; k < tmp2.size(); k++) {
-                        TreeNode* c = new TreeNode(i);
-                        c->left = tmp1[j];
-                        c->right = tmp2[k];
-                        result.push_back(c);
-                    }
-                }
+              }
             }
         }
         return result;
     }
-    
+
     vector<TreeNode *> generateTrees(int n) {
-        if (n == 0) {
-            vector<TreeNode* > tmp;
-            tmp.push_back(NULL);
-            return tmp;
-        }
         return gen(1, n);
-        
     }
 };
