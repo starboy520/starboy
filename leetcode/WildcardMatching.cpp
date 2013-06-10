0be22e9fd91396303eea4db7f43c3be13c7e53c3
qichengjie <starboy.qi@gmail.com>
2013-06-10 22:22:28 +0800

    Update WildcardMatching.cpp

--- leetcode/WildcardMatching.cpp
+++ leetcode/WildcardMatching.cpp
@@ -81,3 +81,24 @@ public:
     }
 };
 */
+
+/*
+class Solution {
+public:
+    bool isMatch(const char *s, const char *p) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        if (*p == '*'){//return true;
+            while(*p == '*') ++p;
+            if (*p == '\0') return true;
+            while(*s != '\0' && !isMatch(s,p)){
+                ++s;                
+            }
+            return *s != '\0';
+        }
+        else if (*p == '\0' || *s == '\0') return *p == *s;
+        else if (*p == *s || *p == '?') return isMatch(++s,++p);
+        else return false;
+    }
+};
+*/
