f87e472e3619a8d4a0d04d98c09c78144075bc66
qichengjie <starboy.qi@gmail.com>
2013-06-11 13:15:20 +0800

    Update WordLadder.cpp

--- leetcode/WordLadder.cpp
+++ leetcode/WordLadder.cpp
@@ -1,82 +1,38 @@
-#include <string>
-#include <unordered_set>
-using namespace std;
-
-/*
-    public int ladderLength(String start, String end, HashSet<String> dict) {
-         if (start == null || end == null || start.length() != end.length())
-             return 0;
-         int dist = 1;
-         HashSet<String> used = new HashSet<String>();
-         HashSet<String> cur = new HashSet<String>();
-         cur.add(start);
-         while(!cur.isEmpty()) {
-             HashSet<String> newCur = new HashSet<String>();
-             dist++;
-             for (String s : cur) {
-                 char[] str = s.toCharArray();
-                 for (int i = 0; i < str.length; i++) {
-                     for (int j = 0; j < 26; j++) {
-                         if (str[i] != (char)('a' + j)) {
-                             char temp = str[i];
-                             str[i] = (char)('a' + j);
-                             String candidate = new String(str);
-                             if (candidate.equals(end))
-                                 return dist;
-                             if (!used.contains(candidate) && dict.contains(
- candidate)) {
-                                 newCur.add(candidate);
-                                 used.add(candidate);
-                             }
-                             str[i] = temp;
-                         }
-                     }
-                 }
-             }
-
-             cur = newCur;
-         }
-
-         return 0;
-     }
-
-*/
-
 class Solution {
-	public:
-		int ladderLength(string start, string end, unordered_set<string> dict) {
-			if (start == "" || end == "" || start.length() != end.length()) {
-				return 0;
-			}
-
-			int dist = 1;
-			unordered_set<string> used;
-			unordered_set<string> cur;
-			cur.insert(start);
-
-			while (!cur.empty()) {
-				unordered_set<string> newCur;
-				dist++;
-
-				for (unordered_set<string>::iterator it = cur.begin(); it != cur.end(); it++) {
-					for (int i = 0; i < (*it).size(); i++) {
-						for (int j = 0; j < 26; j++) {
-							if ((*it)[i] != ('a' + j)) {
-								string s = *it;
-								char temp = s[i];
-								s[i] = ('a' + j);
-								if (s == end) return dist;
-								if (used.find(s) == used.end() && dict.find(s) != dict.end()) {
-									newCur.insert(s);
-									used.insert(s);
-								}
-								s[i] = temp;
-							}
-						}
-					}
-				}
-				cur = newCur;
-			}
-			return 0;
-		}
+public:
+    int ladderLength(string start, string end, unordered_set<string> &dict) {
+        unordered_set<string> visited;
+        queue<string> qu;
+        
+        
+        int count = 1;
+        string FLAG;
+        visited.insert(start);
+        qu.push(start);
+        qu.push(FLAG);
+        
+        while (!qu.empty()) {
+            string word = qu.front();
+            qu.pop();
+            
+            if (word == end) {
+                return count;
+            } else if (word == FLAG && !qu.empty()) {
+                qu.push(FLAG);
+                count++;
+            } else if (!word.empty()) {
+                for (int i = 0; i < start.size(); i++) {
+                    string tmp = word;
+                    for (char c = 'a' ;c <= 'z'; c++) {
+                        tmp[i] = c;
+                        if (dict.find(tmp) != dict.end() && visited.find(tmp) == visited.end()) {
+                            qu.push(tmp);
+                            visited.insert(tmp);
+                        }
+                    }
+                }
+            }
+        }
+        return 0;
+    }
 };
