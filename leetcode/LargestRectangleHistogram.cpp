22fb019061ffbdf6030816c2f1428284d2a41a27
qichengjie <starboy.qi@gmail.com>
2013-06-11 19:41:53 +0800

    Update LargestRectangleHistogram.cpp

--- leetcode/LargestRectangleHistogram.cpp
+++ leetcode/LargestRectangleHistogram.cpp
@@ -32,3 +32,34 @@ public:
         return maxars;
     }
 };
+
+
+class Solution {
+public:
+    int largestRectangleArea(vector<int> &height) {
+        int len = height.size();
+        vector<int> left(len, 0);
+        vector<int> right(len, 0);
+        
+        for (int i = 0; i < len; i++) {
+            left[i] = i;
+            while (left[i] >= 1 && height[left[i]-1] >= height[i]) {
+                left[i] = left[left[i]-1];
+            }
+        }
+        
+        for (int i = len-1; i >= 0; i--) {
+            right[i] = i;
+            while (right[i] <= len-2 && height[i] <= height[right[i]+1]) {
+                right[i] = right[right[i]+1];
+            }
+        }
+        
+        int m = 0;
+        for (int i = 0; i < len; i++) {
+            int area = (right[i] - left[i]+1) * height[i];
+            m = max(m, area);
+        }
+        return m;
+    }
+};
