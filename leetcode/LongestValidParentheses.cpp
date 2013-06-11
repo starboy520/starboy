9ed294bdfe41510f725ad42da274b5f0d3e74de7
qichengjie <starboy.qi@gmail.com>
2013-06-11 16:42:42 +0800

    Update LongestValidParentheses.cpp

--- leetcode/LongestValidParentheses.cpp
+++ leetcode/LongestValidParentheses.cpp
@@ -30,3 +30,30 @@ public:
         
     }
 };
+
+/**
+ * 
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int n = s.length();
+        vector<int> dp(n, 0);
+        
+        int m = 0;
+        for (int i = n-2; i >= 0; i--) {
+            if (s[i] == '(') {
+                int j = i+1 + dp[i+1];
+                if (j < n && s[j] ==')') {
+                    dp[i] = dp[i+1]+2;
+                    if (j +1 < n) {
+                        dp[i] += dp[j+1];
+                    }
+                }
+                m = max(m, dp[i]);
+            }
+        }
+        return m;
+    }
+};
+
+*/
