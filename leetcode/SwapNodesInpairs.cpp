f353f4095cfbea866fea2875f8837522e98eed03
qichengjie <starboy.qi@gmail.com>
2013-06-11 15:26:07 +0800

    Update SwapNodesInpairs.cpp

--- leetcode/SwapNodesInpairs.cpp
+++ leetcode/SwapNodesInpairs.cpp
@@ -31,4 +31,36 @@ public:
         }
         return ret_head == NULL? head:ret_head;
     }
+    
+    
 };
+
+/*
+
+class Solution {
+public:
+    ListNode *swapPairs(ListNode *head) {
+        if (head == NULL) return NULL;
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode* prev = &dummy;
+        ListNode* cur = head;
+        while (cur != NULL) {
+            ListNode* first = cur;
+            ListNode* second = cur->next;
+            if (second == NULL) {
+                prev->next = first;
+                return dummy.next;
+            }
+            cur = second->next;
+            prev->next = second;
+            second->next = first;
+            prev = first;
+            prev->next = NULL;
+            
+        }
+        return dummy.next;
+    }
+};
+
+*/
